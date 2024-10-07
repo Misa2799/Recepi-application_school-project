578b517b6cf04f71c3de3326eb2032a0f2da2588
Tomas <44404123+tomeksen@users.noreply.github.com>
2024-10-07 11:20:29 -0700

    deploy of the project

---
+++ frontend/zaragoza-project/src/types/types.ts
@@ -0,0 +1,27 @@
+export interface recipe{
+    id: number,
+    name: string,
+    description: string,
+    ingredients: string,
+    steps: string,
+    image: string,
+    category: string
+}
+
+export interface category{
+    id: number,
+    name: string
+}
+
+export interface user{
+    id: number,
+    username: string,
+    password: string
+}
+
+export interface groceryCart{
+    id: number,
+    user_id: number,
+    recipe_id: number,
+    quantity: number
+}
