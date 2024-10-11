f218d1ddd72422e4672da53dc3dd7329137cef8a
Tomas <44404123+tomeksen@users.noreply.github.com>
2024-10-11 09:35:32 -0700

    few changes

--- frontend/zaragoza-project/src/app/page.tsx
+++ frontend/zaragoza-project/src/app/page.tsx
@@ -4,22 +4,22 @@ import SideBar from "@/components/sideBar";
 import { useEffect, useState } from "react";
 import { getRecipes, addRecipes, deleteRecipes } from '@/app/actions'
 import { Recipe } from "@/types/types";
+import Header from "@/components/header";
 
 export default function Home() {
-  // const [ recipes, setRecipes ] = useState<recipe[]>([])
-  // useEffect(() => {
-  //   fetchRecipes()
-  // }, [])
+  const [ recipes, setRecipes ] = useState<Recipe[]>([])
+  useEffect(() => {
+    fetchRecipes()
+  }, [])
 
-  // const fetchRecipes = async () => {
-  //   const fetchedTasks = await getRecipes()
-  //   setRecipes(fetchedTasks)
-  // }
+  const fetchRecipes = async () => {
+    const fetchedTasks = await getRecipes()
+    setRecipes(fetchedTasks)
+  }
 
   return (
     <div className="flex flex-1 p-4 justify-between">
-      <SideBar />
-      <RecipesSideBar />
+      
     </div>
   );
 }
--- frontend/zaragoza-project/src/models/wishlist.ts
+++ frontend/zaragoza-project/src/models/wishlist.ts
@@ -5,5 +5,11 @@ const WishlistSchema = new Schema({
     userId: { type: String, required: true },
     recipeId: [{ type: String, required: true }],
 })
+
+export interface WishlistInterface {
+    id?: string;
+    userId: string;
+    recipeId: string[];
+}
   
 export default mongoose.models.Wishlist || mongoose.model('Wishlist', WishlistSchema)
