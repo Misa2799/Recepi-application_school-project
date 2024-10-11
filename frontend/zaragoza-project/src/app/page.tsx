0078e9937a52d6e55806a80b4bb6a7d4b932c891
Tomas <44404123+tomeksen@users.noreply.github.com>
2024-10-11 09:40:20 -0700

    adding yui home

--- frontend/zaragoza-project/src/app/page.tsx
+++ frontend/zaragoza-project/src/app/page.tsx
@@ -2,7 +2,7 @@
 import RecipesSideBar from "@/components/recipesSideBar";
 import SideBar from "@/components/sideBar";
 import { useEffect, useState } from "react";
-import { getRecipes, addRecipes, deleteRecipes } from '@/app/actions'
+import { getRecipesList} from '@/app/actions'
 import { Recipe } from "@/types/types";
 import Header from "@/components/header";
 
@@ -13,13 +13,17 @@ export default function Home() {
   }, [])
 
   const fetchRecipes = async () => {
-    const fetchedTasks = await getRecipes()
-    setRecipes(fetchedTasks)
+    const fetchedTasks = await getRecipesList()
+    setRecipes(fetchedTasks.recipes)
   }
 
   return (
-    <div className="flex flex-1 p-4 justify-between">
-      
+    <div className="">
+      { recipes.map((recipe) => (
+            <div key={recipe.id}>
+                <h3>{recipe.name}</h3>
+            </div>
+        ))}
     </div>
   );
 }
